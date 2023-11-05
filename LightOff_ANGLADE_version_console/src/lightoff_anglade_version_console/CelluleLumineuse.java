6761f7e8902c7347295267429922d5c557a810d1
marine anglade <marine.anglade@epfedu.fr>
2023-11-05 16:48:15 +0100

    mini projet de base fonctionnel

--- LightOff_ANGLADE_version_console/src/lightoff_anglade_version_console/CelluleLumineuse.java
+++ LightOff_ANGLADE_version_console/src/lightoff_anglade_version_console/CelluleLumineuse.java
@@ -10,6 +10,15 @@ package lightoff_anglade_version_console;
  */
 public class CelluleLumineuse {
    private boolean allume;  
+public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_BLACK = "\u001B[30m";
+public static final String ANSI_RED = "\u001B[31m";
+public static final String ANSI_GREEN = "\u001B[32m";
+public static final String ANSI_YELLOW = "\u001B[33m";
+public static final String ANSI_BLUE = "\u001B[34m";
+public static final String ANSI_PURPLE = "\u001B[35m";
+public static final String ANSI_CYAN = "\u001B[36m";
+public static final String ANSI_WHITE = "\u001B[37m";
 
     /**
      * // constructeur qui met automatiquement la valeur de allumé à false
@@ -63,11 +72,11 @@ public class CelluleLumineuse {
     @Override
     public String toString() { 
         if (allume == false){  // test pour savoir si la cellule est éteinte ou allumée
-            return "0";
+            return ANSI_PURPLE +"0" + ANSI_RESET;
             
         }
         else 
-            return "X";
+            return ANSI_CYAN +"X"+ANSI_RESET;
     }
     
 }
